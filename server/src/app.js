1687fe2ab198ace7daa717a95ea81daf51a3277e
aivan <haivan.villegas@gmail.com>
2022-03-24 12:12:52 -0600

    added morgan middlewware for logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+app.use(morgan('combined'))
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'..','public')))
 
