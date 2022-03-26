a74be454d9061e9156105b5f68f24ca169424e82
aivan <haivan.villegas@gmail.com>
2022-03-26 11:17:18 -0600

    Added Data acces layer for better undestanding

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
-const {planets} = require('../../models/planets.model.js');
+const {getAllPlanets} = require('../../models/planets.model.js');
 
-function getAllPlanets(req,res) {
-    return res.status(200).json(planets)
+function httpGetAllPlanets(req,res) {
+    return res.status(200).json(getAllPlanets())
 }
 
-module.exports = {getAllPlanets}
+module.exports = {httpGetAllPlanets}
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const {getAllPlanets} = require('./planets.controller.js');
+const {httpGetAllPlanets} = require('./planets.controller.js');
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', getAllPlanets)
+planetsRouter.get('/planets', httpGetAllPlanets)
 
 module.exports = planetsRouter
