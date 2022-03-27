d872cd8107f606ad2ccf636a3705d7c3d71f9b88
aivan <haivan.villegas@gmail.com>
2022-03-27 10:52:50 -0600

    Connected POST launches with client dashboard

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -16,8 +16,22 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+      return await fetch(`${API_URL}/launches`,{
+      method:'post',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(launch),
+    })
+  } catch (error) {
+    return {
+      ok:false
+    }
+  }
+
+  
 }
 
 async function httpAbortLaunch(id) {
