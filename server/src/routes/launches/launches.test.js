bc9a371b95feddd21721de6e8751b0abd8e69495
aivan <haivan.villegas@gmail.com>
2022-03-29 09:53:28 -0600

    Added some tests to the /launch endpoint

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,75 @@
+
+const request = require('supertest');
+const { response } = require('../../app.js');
+const app = require('../../app.js');
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app).get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200)
+        // expect(response.statusCode).toBe(200)
+    })
+})
+
+describe('test POST /launch', () => {
+
+    const launchData = {
+        mission:'USS',
+        rocket:'NCC 10701D',
+        target:'Kepler4',
+        launchDate:'January 3, 2029'
+    }
+
+    const launchDataNoDate = {
+        mission:'USS',
+        rocket:'NCC 10701D',
+        target:'Kepler4',
+    }
+
+    const launchDataInvalidDate = {
+        mission:'USS',
+        rocket:'NCC 10701D',
+        target:'Kepler4',
+        launchDate:'X'
+    }
+
+    test('It should respond with 201 success',async() => {
+        const res = await request(app)
+            .post('/launches')
+            .send(launchData)
+            .expect('Content-Type',/json/)
+            .expect(201)
+        
+        const reqDate = new Date(launchData.launchDate).valueOf();
+        const resDate = new Date(res.body.launchDate).valueOf();
+
+        expect(reqDate).toBe(resDate)
+
+        expect(res.body).toMatchObject(launchDataNoDate)
+    })
+
+    test('It should catch missing required properties',async () => {
+        const res = await request(app)
+            .post('/launches')
+            .send(launchDataNoDate)
+            .expect('Content-Type',/json/)
+            .expect(400)
+        
+        expect(res.body).toStrictEqual({
+            error:'Missing required launch property'
+        })
+    })
+
+    test('It should catch invalid dates',async() => {
+        const res = await request(app)
+            .post('/launches')
+            .send(launchDataInvalidDate)
+            .expect('Content-Type',/json/)
+            .expect(400)
+        
+        expect(res.body).toStrictEqual({
+            error:'Invalid launch date'
+        })
+    })
+})
--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ async function startServer(){
     await loadPlanetsData();
 
     server.listen(PORT , () => {
-        `Listening on port ${PORT}...`
+        console.log(`Listening on port ${PORT}...`)
     })
 }
 
