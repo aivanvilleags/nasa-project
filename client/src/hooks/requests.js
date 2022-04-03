756391be0fabf3016f8e993c4bbed3d1851c4c8a
aivan <haivan.villegas@gmail.com>
2022-04-03 18:53:22 -0500

    added connection to spacex api using axios and maping the data to this project api

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,57 +1,49 @@
-const API_URL = 'http://localhost:8000'
+const API_URL = "http://localhost:8000/v1";
 
 async function httpGetPlanets() {
   // Load planets and return as JSON.
-  const response = await fetch(`${API_URL}/planets`)
-  return await response.json()
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 async function httpGetLaunches() {
   // Load launches, sort by flight number, and return as JSON.
-  const response = await fetch(`${API_URL}/launches`)
-  const fetchedLaunches =  await response.json()
-  return fetchedLaunches.sort((a,b) => {
-    return a.flightNumber - b.flightNumber
-  })
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 async function httpSubmitLaunch(launch) {
   // Submit given launch data to launch system.
   try {
-      return await fetch(`${API_URL}/launches`,{
-      method:'post',
-      headers:{
-        "Content-Type":"application/json"
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(launch),
-    })
+      body: JSON.stringify(launch),
+    });
   } catch (error) {
     return {
-      ok:false
-    }
+      ok: false,
+    };
   }
-
-  
 }
 
 async function httpAbortLaunch(id) {
   // Delete launch with given ID.
   try {
-      return await fetch(`${API_URL}/launches/${id}`,{
-      method:'delete'
-    })
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
   } catch (error) {
     console.log(error);
-    return{
-      ok:false
-    }
+    return {
+      ok: false,
+    };
   }
-  
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,23 +1,29 @@
 const launches = require("./launches.mongo");
 const planets = require("./planets.mongo");
+const axios = require("axios");
+const launchesMongo = require("./launches.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 0;
 
 const launch = {
-  flightNumber: 0,
-  mission: "Mission X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-442 b",
-  customers: ["ANASA", "ZTM"],
-  upcoming: true,
-  success: true,
+  flightNumber: 0, // flight_number
+  mission: "Mission X", // name
+  rocket: "Explorer IS1", //rocket.name
+  launchDate: new Date("December 27, 2030"), //date_local
+  target: "Kepler-442 b", // not applicable
+  customers: ["ANASA", "ZTM"], // payload.customers for each payload
+  upcoming: true, // upcoming
+  success: true, // success
 };
 
 saveLaunch(launch);
 
+async function findLaunch(filter) {
+  return await launchesMongo.findOne(filter);
+}
+
 async function existsLaunchId(id) {
-  return await launches.findOne({
+  return await findLaunch({
     flightNumber: id,
   });
 }
@@ -40,6 +46,67 @@ async function getAllLaunches() {
       __v: 0,
     }
   );
+}
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
+
+async function populateLaunches() {
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+
+    console.log(launch);
+    // TODO populate launches collection
+  }
+}
+
+async function loadLaunchesData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+
+  if (firstLaunch) {
+    console.log("Launch data alredy loaded!");
+  } else {
+    await populateLaunches();
+  }
 }
 
 async function saveLaunch(launch) {
@@ -90,6 +157,7 @@ async function abortLaunchById(id) {
 }
 
 module.exports = {
+  loadLaunchesData,
   existsLaunchId,
   getAllLaunches,
   scheduleNewLaunch,
