3ab27dc43d1bbe6412e84dd2cab8b8340fadf03c
aivan <haivan.villegas@gmail.com>
2022-04-17 18:29:50 -0500

    added mongodb to github workflow and loadPlanets to testing

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,13 @@
 const request = require("supertest");
 const { response } = require("../../app.js");
 const app = require("../../app.js");
+const { loadPlanetsData } = require("../../models/planets.model.js");
 const { mongoConnect, mongoDisconnect } = require("../../services/mongo.js");
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
